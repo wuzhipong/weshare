45cf9169e535f69a8869789d29b3acf95dc25879
wu <30486261+wuzhipong@users.noreply.github.com>
2020-07-23 19:52:49 +0800

    Update config.php

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -2,8 +2,8 @@
 return array(
 	//'配置项'=>'配置值'
 	'ALIOSS_CONFIG'          => array(
-        'KEY_ID'             => 'LTAINu2Fx7NPjRG8', // 阿里云oss key_id
-        'KEY_SECRET'         => 'BtefMe1LF3jgEioXEGYkrGqnk6n6A5', // 阿里云oss key_secret
+        'KEY_ID'             => '*', // 阿里云oss key_id
+        'KEY_SECRET'         => '*', // 阿里云oss key_secret
         'END_POINT'          => 'https://upload-download.oss-cn-beijing.aliyuncs.com', // 阿里云oss endpoint
         'BUCKET'             => 'upload-download'  // bucken 名称
         ),
